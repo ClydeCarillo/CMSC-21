e7a5cbf9915537adb42e2929fd7668770366cdde
Clyde <75878247+ClydeCarillo@users.noreply.github.com>
2025-03-13 23:43:04 +0800

    Update Machine Problem 5.2 - BOLANDO.c

--- Machine Problem 5.2 - BOLANDO.c
+++ Machine Problem 5.2 - BOLANDO.c
@@ -1,3 +1,23 @@
+/*
+Implement a program that sorts the elements of an array in ascending order.
+
+Have an array of size 10 and enter the values for each of its elements.
+Then, create a void function that will be used to sort the values of the array in ascending order.
+
+Display the sorted array in the output.
+
+The use of any [ ] in the code is PROHIBITED, except to initialize an array. Use pointers and pointer arithmetic instead.
+
+
+SAMPLE
+
+Input:     98 67 34 89 -23 83 -9 27 11 10
+Output:  -23 -9 10 11 27 34 67 83 89 98
+
+Input:     1 0 2 9 3 8 4 7 5 6
+Output: 0 1 2 3 4 5 6 7 8 9
+*/
+
 #include <stdio.h>
 
 void sortArray(int *arr, int size) {
